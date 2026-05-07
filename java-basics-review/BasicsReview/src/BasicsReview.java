fec977b1dfff78b5326d6057f8ead1142bd35d36
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-05-07 16:46:40 -0600

    Learning Java: Basics Review: Variables

---
+++ java-basics-review/BasicsReview/src/BasicsReview.java
@@ -0,0 +1,14 @@
+public class BasicsReview {
+
+    public static void main(String[] args) {
+        String myVariable = "Example Value";
+        System.out.println(
+                "The value of my variable is: " + myVariable
+        );
+
+        Integer myInt = 3;
+        System.out.println(
+                "The value of my integer is: " + myInt
+        );
+    }
+}
