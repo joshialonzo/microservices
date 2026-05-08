79d9397a0ab7c1ae173faf525217a54d980a5456
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-05-07 19:17:18 -0600

    Learning Java: Basics Review: Arrays

--- java-basics-review/BasicsReview/src/BasicsReview.java
+++ java-basics-review/BasicsReview/src/BasicsReview.java
@@ -13,9 +13,21 @@ public class BasicsReview {
 
         BasicsReview basics = new BasicsReview();
         System.out.println(basics.addNumbers(2, 4));
+
+        basics.printArray();
     }
 
     int addNumbers(int num1, int num2) {
         return num1 + num2;
+    }
+
+    void printArray() {
+        String[] myArray = new String[3];
+        myArray[0] = "Hi";
+        myArray[1] = "I'm";
+        myArray[2] = "Joshi";
+        for (String value : myArray) {
+            System.out.print(value + " ");
+        }
     }
 }
